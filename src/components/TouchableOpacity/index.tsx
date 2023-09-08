7d9f3f684735ddcd11fe1feae1550e9d4af53c40
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-09-08 18:07:35 -0300

    refactor: Remove estilos personalizados não necessário

--- src/components/TouchableOpacity/index.tsx
+++ src/components/TouchableOpacity/index.tsx
@@ -1,15 +1,5 @@
 import styled from "styled-components/native";
 
-interface TouchableOpacityProps {
-  font?: "R400" | "I400" | "I700";
-  color?: string;
-  size?: number;
-  opacity?: number;
-}
+interface TouchableOpacityProps {}
 
-export const TouchableOpacity = styled.TouchableOpacity<TouchableOpacityProps>`
-  font-family: ${({ font }) => (font ? `${font}` : "I400")};
-  color: ${({ color }) => color || "#000"};
-  font-size: ${({ size }) => (size ? `${size}px` : "16px")};
-  opacity: ${({ opacity }) => opacity || 1};
-`;
+export const TouchableOpacity = styled.TouchableOpacity<TouchableOpacityProps>``;

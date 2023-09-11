d31a82a8c19ba834da8e046d8b86a5ecc24f78f3
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-09-11 20:23:59 -0300

    feat: Adiciona configuração de fontes e de navegação de páginas

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import {
+	useFonts,
+	Inter_400Regular as I400,
+	Inter_700Bold as I700,
+} from "@expo-google-fonts/inter";
+import { Roboto_400Regular as R400 } from "@expo-google-fonts/roboto";
+import { theme } from "./src/styles/theme";
+
+import { Home } from "./src/pages/Home";
+import { Details } from "./src/pages/Details";
+import { Theme } from "./src/types/theme";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const [fontsLoaded] = useFonts({
+		I400,
+		I700,
+		R400,
+	});
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	if (!fontsLoaded) {
+		return null;
+	}
+
+	const Stack = createStackNavigator();
+
+	return (
+		<ThemeProvider theme={theme as Theme}>
+			<StatusBar style="auto" translucent={true} />
+			<NavigationContainer>
+				<Stack.Navigator initialRouteName="Home">
+					<Stack.Screen
+						options={{
+							title: "",
+							headerTransparent: false,
+							headerShown: false,
+						}}
+						name="Home"
+						component={Home}
+					/>
+					<Stack.Screen
+						options={{
+							title: "",
+							headerTransparent: false,
+							headerShown: false,
+						}}
+						name="Details"
+						component={Details}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
+		</ThemeProvider>
+	);
+}

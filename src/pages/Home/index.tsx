b4f85e250e1e0c74345949983a6dff2d9014e7b2
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-09-11 20:54:51 -0300

    feat: Adiciona componente MovieDetails na página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import { Container, Content } from "./styles";
 
 import { Header } from "../../components/Header";
+import { MovieDetails } from "../../components/MovieDetails";
 
 export const Home = ({ navigation }: { navigation: any }) => {
 	return (
 		<Container>
 			<Content>
 				<Header />
+				<MovieDetails />
 			</Content>
 		</Container>
 	);

7ff9577b2c10a929bacd8d14ba44b931623f331c
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-09-11 20:16:51 -0300

    feat(types): Adiciona para o componente de texto

---
+++ src/types/text.ts
@@ -0,0 +1,8 @@
+export interface TextProps {
+	font?: "R400" | "I400" | "I700";
+	color?: string;
+	size?: number;
+	opacity?: number;
+	textAlign?: "auto" | "center" | "justify" | "left" | "right";
+	padding?: string;
+}

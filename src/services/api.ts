04f83be71104874f23cbf397fe332ff3910ed9ba
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-09-11 18:20:26 -0300

    feat: Adiciona configurações e arquivos png do projeto

---
+++ src/services/api.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+import { TMDB_API_KEY } from "@env";
+
+const API_KEY = TMDB_API_KEY;
+const baseURL = "https://api.themoviedb.org/3";
+
+export const api = axios.create({
+	baseURL,
+	headers: {
+		"Content-Type": "application/json",
+	},
+	params: {
+		api_key: API_KEY,
+	},
+});
---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+	colors: {
+		bg: "#2D2A2A",
+		black: "#000",
+		popularity: "#44BF57",
+		white_200: "#FDFDFD",
+		white_400: "#F4F4F4",
+		whie_600: "#EBE7D3",
+		gray_200: "#76756C",
+		gray_400: "#89887F",
+	},
+};

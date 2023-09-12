623a788833dc93650cb7d4c1d12f1a8876d7825f
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-09-12 17:37:29 -0300

    feat: Adiciona componentes e estilos da página Details

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,10 +1,79 @@
-import { Container, Content } from "./styles";
+import { useState } from "react";
+import {
+	Container,
+	Content,
+	MovieDetails,
+	TouchableOpacity,
+	PlayImage,
+	MovieCover,
+	MovieContent,
+	MovieDescription,
+	MovieInfo,
+} from "./styles";
 import { Text } from "../../components/Text";
+import { MovieProps } from "../../types/movie";
 
-export const Details = () => {
+import { Header } from "../../components/Header";
+import { convertNumberToPercentage } from "../../util/convertNumberToPercentage";
+import { TruncatedText } from "../../components/TruncatedText";
+
+export const Details: React.FC<{
+	route: { params: { movie: MovieProps } };
+}> = ({ route }) => {
+	const { movie } = route.params;
+	const [isPressed, setIsPressed] = useState(false);
+
+	const handlePressIn = () => {
+		setIsPressed(!isPressed);
+	};
+
 	return (
 		<Container>
-			<Content></Content>
+			<Content>
+				<Header />
+				<MovieDetails>
+					<TouchableOpacity onPress={handlePressIn}>
+						{isPressed && (
+							<PlayImage source={require("../../assets/play.png")} />
+						)}
+
+						<MovieCover
+							source={{
+								uri: `https://image.tmdb.org/t/p/original${movie?.backdrop_path}`,
+							}}
+						/>
+					</TouchableOpacity>
+
+					<MovieContent>
+						<Text color="#FDFDFD" size={24} font="I400">
+							{movie?.title}
+						</Text>
+
+						<MovieDescription>
+							<MovieInfo>
+								<Text color="#44BF57" size={12} font="I400">
+									{convertNumberToPercentage(movie?.vote_average as number)}%
+									gostaram
+								</Text>
+								<Text color="#FDFDFD" size={12} font="I400">
+									{movie?.release_date.slice(0, 4)}
+								</Text>
+							</MovieInfo>
+
+							<TruncatedText
+								color="#FDFDFD"
+								size={16}
+								font="I400"
+								textAlign="auto"
+								text={
+									movie?.overview !== "" ? movie?.overview : "Não há sinopse."
+								}
+								maxChars={66}
+							/>
+						</MovieDescription>
+					</MovieContent>
+				</MovieDetails>
+			</Content>
 		</Container>
 	);
 };
--- src/pages/Details/styles.ts
+++ src/pages/Details/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
 
 import { statusBarHeight } from "../../util/getStatusBarHeight";
+import { Theme } from "../../types/theme";
 
 export const Container = styled.SafeAreaView`
 	flex: 1;
@@ -9,4 +10,40 @@ export const Container = styled.SafeAreaView`
 
 export const Content = styled.ScrollView`
 	flex: 1;
+	background-color: ${({ theme }: { theme: Theme }) => theme.colors.bg};
+`;
+
+export const TouchableOpacity = styled.TouchableOpacity`
+	flex: 1;
+	align-items: center;
+	justify-content: center;
+`;
+
+export const MovieDetails = styled.View`
+	padding-bottom: 32px;
+`;
+
+export const PlayImage = styled.Image`
+	position: absolute;
+	z-index: 100;
+`;
+
+export const MovieCover = styled.Image`
+	min-width: 100%;
+	height: 192px;
+`;
+
+export const MovieContent = styled.View`
+	padding: 8px 30px 0px;
+`;
+
+export const MovieDescription = styled.View`
+	margin-top: 1px;
+	gap: 4px;
+`;
+
+export const MovieInfo = styled.View`
+	flex-direction: row;
+	align-items: center;
+	gap: 16px;
 `;

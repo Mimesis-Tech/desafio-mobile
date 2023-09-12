ba014f4728360f4b50e66096bc9bac2176dd2637
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-09-12 13:45:50 -0300

    feat: Adiciona componente de listagem de filme por categoria

---
+++ src/components/MovieCategory/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View``;
+
+export const FlatList = styled.FlatList`
+	padding: 12px 0 0 20px;
+`;
+
+export const MovieItem = styled.View`
+	flex-direction: "row";
+	align-items: "center";
+	margin: 0 18px 0 0;
+`;
+
+export const Poster = styled.Image`
+	width: 122px;
+	height: 175px;
+	border-radius: 4px;
+`;
+
+export const TouchableOpacity = styled.TouchableOpacity``;
